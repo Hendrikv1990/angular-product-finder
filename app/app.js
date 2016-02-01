da6d1b823a85ea3ac37767bf4d4f9edaf22638d4
Jean Duthon <duthon.jean@gmail.com>
2016-02-01 17:50:29 +0000

    Fixing some previous mistakes + handling slide & option stack

--- app/app.js
+++ app/app.js
@@ -4,9 +4,36 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.productFinder',
-  'myApp.view2',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/productFinder'});
-}]);
+}])
+
+    .directive('bgImg', [function () {
+      return {
+        'restrict': 'A',
+        'scope': true,
+        'link': function ($scope, element, attrs) {
+
+          $scope.setBg = function (srcImg) {
+
+            if (!!!srcImg) {
+
+              element[0].style.backgroundImage =  'url(' + attrs.bgSrc + ') ';
+
+            } else {
+
+              element[0].style.backgroundImage =  'url(' + srcImg + ') ';
+            }
+
+            element[0].style.backgroundRepeat = attrs.bgRepeat;
+            element[0].style.backgroundSize = attrs.bgSize;
+            element[0].style.backgroundAttachment = attrs.bgAttachment;
+          };
+
+          $scope.setBg();
+        }
+      };
+
+    }]);
--- app/productFinder/productFinder.js
+++ app/productFinder/productFinder.js
@@ -1,5 +1,21 @@
 'use strict';
 
+function findById(arr,id){
+  return arr.filter(function(item){
+    return item._id == id;
+  })[0];
+}
+
+
+function getNextSlide($scope,optionId){
+  var currentPath = $scope.slidesPath;
+  $scope.actionStack.forEach(function(item){
+    currentPath = currentPath.options[item.option];
+  });
+
+  return findById($scope.slidesList,currentPath.options["" + optionId]._id);
+}
+
 angular.module('myApp.productFinder', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,6 +25,24 @@ angular.module('myApp.productFinder', ['ngRoute'])
   });
 }])
 
-.controller('ProductFinderCtrl', [function() {
+.controller('ProductFinderCtrl',
+  function($scope,$http) {
+    $scope.actionStack = [];
 
-}]);
+    $scope.findById = findById;
+
+    $http.get("data_test/test_data.json")
+        .then(function(response) {
+          $scope.optionsList = response.data.optionsList;
+          $scope.slidesList = response.data.slidesList;
+          $scope.slidesPath = response.data.slidesPath;
+          $scope.initialSlide = response.data.initialSlide;
+          $scope.currentSlide = findById($scope.slidesList,$scope.initialSlide);
+      });
+
+    $scope.optionClicked = function(optionId){
+      var oldSlideId = $scope.currentSlide._id;
+      $scope.currentSlide = getNextSlide($scope,optionId);
+      $scope.actionStack.push({"slideId" : oldSlideId,"option":"" + optionId});
+    };
+});
